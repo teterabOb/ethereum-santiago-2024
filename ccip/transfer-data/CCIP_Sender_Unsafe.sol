98c3cd1718f420516c30f359f3b4384260d5ac47
Gilberts Ahumada <gilbertsahumada@gmail.com>
2024-10-16 23:56:29 -0300

    AVAX Summit Update

--- ccip/transfer-data/CCIP_Sender_Unsafe.sol
+++ ccip/transfer-data/CCIP_Sender_Unsafe.sol
@@ -1,29 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity 0.8.24;
 
-import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
-import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
-contract CCIPSender_Unsafe {
-    address link;
-    address router;
+contract Messenger {
+    IERC20 private s_linkToken;
+    IRouterClient private s_router;
 
-    constructor(address _link, address _router) {
-        link = _link;
-        router = _router;
-        LinkTokenInterface(link).approve(router, type(uint256).max);
+    constructor(address _router, address _link) {
+        s_linkToken = IERC20(_link);
+        s_router = IRouterClient(_router);
     }
-    
-    function send(address receiver, string memory someText, uint64 destinationChainSelector) external {
-        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
-            receiver: abi.encode(receiver),
-            data: abi.encode(someText),
+
+    function send(
+        uint64 _destinationChainSelector,
+        address _receiver,
+        string calldata _text
+    ) external {
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver),
+            data: abi.encode(_text),
             tokenAmounts: new Client.EVMTokenAmount[](0),
             extraArgs: "",
-            feeToken: link
+            feeToken: address(s_linkToken)
         });
 
-        IRouterClient(router).ccipSend(destinationChainSelector, message);
+        s_linkToken.approve(address(s_router), 1e18 ether);
+        s_router.ccipSend(_destinationChainSelector, evm2AnyMessage);
     }
 }
